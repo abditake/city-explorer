3ba9ec020596bcb55d133d82543bc6df6d64362c
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-03-22 13:30:19 -0700

    finished lab 6

--- src/Form.js
+++ src/Form.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react'
-import { Form, Button} from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
+
 
 
 export default class Forms extends Component {
-  
+
 
   render() {
-    
-    
+
+
     return (
       <Form onSubmit={this.props.getCityData}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Search for City</Form.Label>
-          <Form.Control onInput={this.props.handleCityInput}/>
-          {this.props.error
-          ?
-          <p>{this.props.errorMessage}</p>
-           :
-           <ul>
-          {this.props.lat === "" ? undefined: <li>{this.props.lat}</li>}
-          
-          {this.props.lon === "" ? undefined: <li>{this.props.lon}</li>}
-          
-          {this.props.display === "" ? undefined: <li>{this.props.display}</li>}
-          <img src={this.props.img} alt={this.props.display}/>
-          </ul> 
-          }
+          <Form.Control onInput={this.props.handleCityInput} />
           <Button variant="primary" type="submit">
             Explore!
           </Button>
---
+++ src/Map.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Card, } from 'react-bootstrap'
+
+export default class Map extends Component {
+  // constructor(props) {
+  //   super(props)
+
+  //   let cityMapUrl = `https://maps.locationiq.com/v3/staticmap?key=<${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.props.lat},${this.props.lon}&zoom=12&size=300x300&format=<format>&maptype=<MapType>&markers=icon:<icon>|${this.props.lat},${this.props.lon}&markers=icon:<icon>|${this.props.lat},${this.props.lon}`
+
+  //   this.setState({
+  //     img:cityMapUrl 
+  //   })
+  // }
+
+
+  render() {
+    console.log(this.props.img);
+    return (
+      <>
+        {/* <ul>
+          {this.props.lat === "" ? undefined : <li>{this.props.lat}</li>}
+
+          {this.props.lon === "" ? undefined : <li>{this.props.lon}</li>}
+
+          {this.props.display === "" ? undefined : <li>{this.props.display}</li>}
+          <img src={this.props.img} alt={this.props.display} />
+        </ul> */}
+        
+        
+        
+          <img src={this.props.img} alt={this.props.display} />
+          <Card className='map'>
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <Card.Text>Lattid{this.props.lat}</Card.Text>
+              <Card.Text> {this.props.lon}</Card.Text>
+            </Card.Body>
+          </Card>
+        </>
+
+
+
+        )
+  }
+}
