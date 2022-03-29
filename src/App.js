4a3bf5d63e10f14d8b5f33d6d8468550d41a95d2
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-03-28 20:28:00 -0700

    lab 7-8

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Forms from './Form';
 import Footer from './Footer';
 import axios from 'axios';
 import Map from './Map';
-import Weather from './Weather.js'
+import Weather from './Weather';
 import Movie from './Movie.js';
 
 
@@ -46,17 +46,18 @@ export default class App extends Component {
       let cityDataLocations = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
 
       let cities = cityDataLocations.data[0];
-      console.log(cities);
+     
 
-      let cityForeCast = await axios.get(`${process.env.REACT_APP_SERVER}weather?searchQueryCity=${this.state.city}}`);
+      let cityForeCast = await axios.get(`${process.env.REACT_APP_SERVER}weather?searchQueryCity=${this.state.city}`);
 
       let cityMovieData = await axios.get(`${process.env.REACT_APP_SERVER}movie?movieQueryCity=${this.state.city}`);
 
       let forecast = cityForeCast.data;
 
       let movieData = cityMovieData.data;
-
-
+      
+      
+      console.log(movieData);
       // save it to state.
       this.setState({
         lat: cities.lat,
@@ -64,8 +65,8 @@ export default class App extends Component {
         display: cities.display_name,
         weatherData: forecast,
         movieDataArray: movieData
-
-
+        
+        
       });
     } catch (error) {
       this.setState({
@@ -75,36 +76,25 @@ export default class App extends Component {
     }
   };
   
-
-
-render() {
-  let cityMapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12&size=300x300`
-
-  console.log(this.state.movieDataArray);
-
-  let foreCastWeatherData = this.state.weatherData.map((a) => {
-    return a.forecast;
-  });
-
-  let foreCastDescData = this.state.weatherData.map((a) => {
-    return a.description;
-  });
-
-
-  return (
-    <>
+  
+  
+  render() {
+    let cityMapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12&size=300x300`
+    
+    return (
+      <>
       <Header/>
       <Forms
         getCityData={this.getCityData}
         handleCityInput={this.handleCityInput}
         error={this.state.error}
         errorMessage={this.state.errorMessage}
-      />
+        />
 
       {this.state.lat ?
         <Map
-          lat={this.state.lat}
-          lon={this.state.lon}
+        lat={this.state.lat}
+        lon={this.state.lon}
           display={this.state.display}
           img={cityMapUrl}
         />
@@ -116,21 +106,19 @@ render() {
       {this.state.lat
         ?
         <Weather
-          foreCastData={foreCastWeatherData}
-          foreCastDescData={foreCastDescData}
-        />
+        weatherData={this.state.weatherData}
+        city={this.state.city}/>
         :
         <>
         </>
       }
       {this.state.movieDataArray
-      ?
-      <Movie
-      movieArray={this.state.movieDataArray}
-      />
-      :
-      <>
-      </>
+        ?
+        <Movie
+        movieArray={this.state.movieDataArray}/>
+        :
+        <>
+        </>
       }
       <Footer />
     </>
--- src/WeatherDay.js
+++ src/WeatherDay.js
@@ -7,12 +7,15 @@ export default class WeatherDay extends Component {
 
   render() {
     return (
+
+      <Container>
         <Card>
           <Card.Body>
-            <Card.Title>{this.props.city} Weather: {this.props.forecast}</Card.Title>
+            <Card.Title>Weather in: {this.props.city} on: {this.props.date}</Card.Title>
             <Card.Text>{this.props.description}</Card.Text>
           </Card.Body>
         </Card>
+      </Container>
     )
   }
 }
